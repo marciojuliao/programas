01ce873b1ce399008f5e64e5edbf10f77b7b51f1
Marcio Juliao <m100juliao@yahoo.com.br>
2015-10-07 10:40:27 -0300

    Meu primeiro Commit

---
+++ gitando.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title>Exemplo com git</title>
+	<style>
+	p{
+		color:red;
+	}
+	</style>
+</head>
+<body>
+	<p>Bom dia!</p>
+	<p style="color:blue">Hello Mundo!</p>
+</body>
+</html>
